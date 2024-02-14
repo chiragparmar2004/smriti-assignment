1a93ab0c7a9c5265fe7bb17009d6ce21d3355fe4
chiragparmar2004 <chiragparmar4780@gmail.com>
2024-02-15 00:38:06 +0530

    changes on api

--- app/api/DemoApi/route.ts
+++ app/api/DemoApi/route.ts
@@ -8,26 +8,26 @@ const convertImageToText = async (imageData: string): Promise<string> => {
 };
 export { convertImageToText };
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method === "POST") {
-    try {
-      // Extract image data from the request body
-      const { imageData } = req.body;
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   if (req.method === "POST") {
+//     try {
+//       // Extract image data from the request body
+//       const { imageData } = req.body;
 
-      // Perform image to text conversion
-      const textData = await convertImageToText(imageData);
+//       // Perform image to text conversion
+//       const textData = await convertImageToText(imageData);
 
-      return res.status(200).json({ textData });
-    } catch (error) {
-      console.error("Error converting image to text:", error);
-      res.status(500).json({ error: "Error converting image to text" });
-    }
-  } else {
-    // Method not allowed
-    res.setHeader("Allow", ["POST"]);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
-}
+//       return res.status(200).json({ textData });
+//     } catch (error) {
+//       console.error("Error converting image to text:", error);
+//       res.status(500).json({ error: "Error converting image to text" });
+//     }
+//   } else {
+//     // Method not allowed
+//     res.setHeader("Allow", ["POST"]);
+//     res.status(405).end(`Method ${req.method} Not Allowed`);
+//   }
+// }
